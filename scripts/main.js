c8c8aee8316816a64e30ffb240f6960f089f3448
Rafael Pedrola <rafinskipg@gmail.com>
2016-01-10 23:35:34 +0100

    Simple and working

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+var poems = [];
+var poemIndex;
+var poemTitle, poemBody;
+var prevButton, nextButton;
+var currentPoemDisplayCounter, totalPoemsDisplayCounter;
+
+function showPoem(index){
+  if(index >= 0 && index < poems.length){
+    poemIndex = index;
+    poemTitle.innerText = poems[index].querySelector('.title').innerText
+    poemBody.innerHTML = poems[index].querySelector('.content').innerHTML
+    updatePoemInfo(poemIndex);
+  }
+}
+
+function initCounter(){
+  currentPoemDisplayCounter = document.querySelector('.at')
+  totalPoemsDisplayCounter = document.querySelector('.total')
+  totalPoemsDisplayCounter.innerText = poems.length;
+}
+
+function updatePoemInfo(index){
+  var realValue = index + 1;
+  currentPoemDisplayCounter.innerText = realValue;
+}
+
+function bindControlsActions(){
+  prevButton = document.querySelector('.prev')
+  nextButton = document.querySelector('.next')
+
+  prevButton.onclick = function(){
+    showPoem(poemIndex - 1);
+  }
+
+  nextButton.onclick = function(){
+    showPoem(poemIndex + 1);
+  }
+
+}
+
+window.onload = function(){
+  poems = document.querySelectorAll('.poem');
+  poemTitle = document.querySelector('.poem-title')
+  poemBody = document.querySelector('.poem-text')
+  
+  initCounter();
+  showPoem(0);
+  bindControlsActions();
+  console.log(poems);
+}
